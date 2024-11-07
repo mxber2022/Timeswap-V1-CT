2cfae22de6abd38a2f75af169e1c3f687c63457f
mxber2022 <mxber2022@gmail.com>
2024-11-07 17:40:56 +0700

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,29 +1,11 @@
 'use client'
 
 import { DynamicWidget } from '../../lib/dynamic'
-import { useEffect, useState } from 'react'
-
-const checkIsDarkSchemePreferred = () => {
-  if (typeof window !== 'undefined') {
-    return window.matchMedia?.('(prefers-color-scheme:dark)')?.matches ?? false
-  }
-  return false
-}
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(checkIsDarkSchemePreferred)
-
-  useEffect(() => {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-    const handleChange = () => setIsDarkMode(checkIsDarkSchemePreferred())
-
-    darkModeMediaQuery.addEventListener('change', handleChange)
-    return () => darkModeMediaQuery.removeEventListener('change', handleChange)
-  }, [])
-
   return (
     <>
-      <div className="modal">
+      <div>
         <DynamicWidget />
       </div>
     </>
